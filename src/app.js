27c0dbd41328c618bce1298d4b2dfdea5aa925ec
serg <5873970+undefined@users.noreply.github.com>
2022-10-06 14:51:40 +0000

    add time frame example

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 import { LightningElement } from "lwc";
 
 export default class App extends LightningElement {
-  title = "Welcome to Lightning Web Components!";
+  title = "Welcome to CIP!";
 
   showFeatures = true;
 
